fa6e16f0a6f89d601846a5b47efb6c836cf8f99c
John Dueno <djohnruther@yahoo.com>
2018-07-07 20:01:49 -0500

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -26,6 +26,16 @@ app.post('/todos', (req, res) => {
 	});
 });
 
+//get('/todos') route 
+//returning all of todos
+app.get('/todos', (req, res) => {
+	Todo.find().then((todos) => {
+		res.send({todos})
+	}, (e) => {
+		res.status(400).send(e);
+	})
+});
+
 //Read
 app.listen(3000, () => {
 	console.log('Started on port 3000');
