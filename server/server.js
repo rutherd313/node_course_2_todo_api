ce0575ed8f54137af2e5b6036159dbcc4eefae2a
John Dueno <djohnruther@yahoo.com>
2018-07-07 21:52:29 -0500

    Add Get /todos/:id

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 //server.js is responsible for routes
-
+const {ObjectID} = require('mongodb')
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user')
@@ -35,6 +35,31 @@ app.get('/todos', (req, res) => {
 		res.status(400).send(e);
 	})
 });
+
+//Call Get todos with specific id
+//url params = :name, this created id var. It will be on the
+//req obj, and will be able to access that variable
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id
+//req.params is an obj with key/value pairs where key is
+//the url param(id) & val is on what is placed on id
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+	//Quering database
+	Todo.findById(id).then((todo) => {
+		if(!todo) {
+			return res.status(404).send();
+		}
+		//success path
+		res.send({todo});
+	}).catch((e) => {
+		res.status(400).send()
+	});
+});
+	
+	
+
 
 //Read
 app.listen(3000, () => {
